e79a285ccb25f4e49b6c33e8c6268048d2b6d8e8
inocelen <ino@celen.jp>
2020-02-06 22:27:10 +0900

    add project10

---
+++ miniapp/project10/main.js
@@ -0,0 +1,31 @@
+/* instagram */
+$(function () {
+    try {
+        this.name = "via_jpn";
+        $.ajax('https://www.instagram.com/' + this.name + '/', {
+            timeout: 2000,
+            datatype: 'html'
+        }).then(function (data) {
+            json_string = data.split("window._sharedData = ")[1];
+            json_string = json_string.split("};</script>")[0] + "}";
+            this.Arrya_data = JSON.parse(json_string);
+            let datas = this.Arrya_data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
+            for (i in datas) {
+                // 画像URL取得
+                var url = datas[i].node.display_url;
+                // テキスト取得
+                var text = datas[i].node.edge_media_to_caption.edges[0].node.text;
+
+                this.html = `
+                <div class="cardImgWrp p-3">
+                <img src="${url}">
+                <p>${text}</p>
+                </div>
+                `;
+                $(".insta-card").append(this.html);
+            }
+        });
+    } catch (error) {
+        alert(error);
+    }
+})
